68307e1febfff7e4b167ea86c5fa160f4cc50a01
cheng ying <chengying@tezign.com>
2020-01-01 20:42:49 +0800

    update

--- training.py
+++ training.py
@@ -71,7 +71,10 @@ def evaluate_model(model):
             loss += criterion(output, mask_batch, regr_batch, size_average=False).item()
 
     loss /= len(valid_loader.dataset)
-    MAP = get_map(model)
+    try:
+        MAP = get_map(model)
+    except Exception as e:
+        MAP = 0.0
     print('Dev loss: {:.4f}, map {}'.format(loss, MAP))
 
     return loss, MAP
@@ -95,12 +98,12 @@ def training(model, optimizer, scheduler, n_epoch):
 
 
 if __name__ == '__main__':
-    model = get_model(Config.model_name)
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
-    # exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=Config.N_EPOCH * len(train_loader) // 3, gamma=0.1)
-    lr_scheduler = ReduceLROnPlateau(optimizer, mode='max', factor=0.1, patience=5, verbose=True)
-    model = training(model, optimizer, scheduler=lr_scheduler, n_epoch=Config.N_EPOCH)
-    predict(model)
+    # model = get_model(Config.model_name)
+    # optimizer = optim.Adam(model.parameters(), lr=0.001)
+    # # exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=Config.N_EPOCH * len(train_loader) // 3, gamma=0.1)
+    # lr_scheduler = ReduceLROnPlateau(optimizer, mode='max', factor=0.1, patience=5, verbose=True)
+    # model = training(model, optimizer, scheduler=lr_scheduler, n_epoch=Config.N_EPOCH)
+    # predict(model)
 
     Config.expriment_id = 2
     Config.model_name = "basic_unet"
