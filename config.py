d321e6b463612364558a11b7858c6d67a7ad188a
cheng ying <chengying@tezign.com>
2020-01-01 12:32:41 +0800

    update

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ class Config():
     IMG_HEIGHT = 320
     IMG_WIDTH = 1024
     MODEL_SCALE = 8
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    PARALLEL = False
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    PARALLEL = True
     device_ids = [0, 1]
     BATCH_SIZE = 4 * len(device_ids) if PARALLEL else 4
     DATA_PATH = './pku-autonomous-driving/'
