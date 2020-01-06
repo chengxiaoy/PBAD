9d4cc613b275ba7d923837dd2f84b869c370d31b
cheng ying <chengying@tezign.com>
2020-01-06 18:24:09 +0800

    test

--- train_v2.py
+++ train_v2.py
@@ -192,7 +192,7 @@ if __name__ == '__main__':
     model = get_model(Config.model_name)
     optimizer = optim.AdamW(model.parameters(), lr=0.001,weight_decay=0.01)
     # exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=Config.N_EPOCH * len(train_loader) // 3, gamma=0.1)
-    lr_scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=3, verbose=True)
+    lr_scheduler = ReduceLROnPlateau(optimizer, mode='max', factor=0.1, patience=3, verbose=True)
     model = training(model, optimizer, scheduler=lr_scheduler, n_epoch=Config.N_EPOCH, writer=writer)
     predict(model)
 
@@ -211,7 +211,7 @@ if __name__ == '__main__':
     model = get_model(Config.model_name)
     optimizer = optim.AdamW(model.parameters(), lr=0.001,weight_decay=0.01)
     # exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=Config.N_EPOCH * len(train_loader) // 3, gamma=0.1)
-    lr_scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=3, verbose=True)
+    lr_scheduler = ReduceLROnPlateau(optimizer, mode='max', factor=0.1, patience=3, verbose=True)
     model = training(model, optimizer, scheduler=lr_scheduler, n_epoch=Config.N_EPOCH, writer=writer)
     predict(model)
 
