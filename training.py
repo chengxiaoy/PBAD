018eb6a0dc9de172549044c6b112fa279e8ed6d8
e <chengyingjuly@163.com>
2020-01-07 23:16:21 +0800

    update

--- training.py
+++ training.py
@@ -141,7 +141,7 @@ if __name__ == '__main__':
     # Config.IMG_WIDTH = 1536
     # Config.IMG_HEIGHT = 512
     Config.FOCAL_ALPHA = 0.75
-    Config.N_EPOCH = 30
+    Config.N_EPOCH = 20
     Config.MASK_WEIGHT = 1
     Config.USE_UNCERTAIN_LOSS = True
     Config.USE_MASK = True
@@ -149,6 +149,27 @@ if __name__ == '__main__':
     uncertain_loss = UncertaintyLoss().to(Config.device)
     params = list(uncertain_loss.parameters()) + list(model.parameters())
     optimizer = optim.Adam(params, lr=0.001)
+    # exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=Config.N_EPOCH * len(train_loader) // 3, gamma=0.1)
+    lr_scheduler = ReduceLROnPlateau(optimizer, mode='max', factor=0.1, patience=3, verbose=True)
+    model = training(model, optimizer, scheduler=lr_scheduler, n_epoch=Config.N_EPOCH, writer=writer,
+                     uncertain_loss=uncertain_loss)
+    predict(model)
+
+    Config.expriment_id = 31
+    writer = SummaryWriter(logdir=os.path.join("board/", str(Config.expriment_id)))
+    Config.model_name = "unet"
+    Config.MODEL_SCALE = 4
+    Config.IMG_WIDTH = 1536
+    Config.IMG_HEIGHT = 512
+    Config.FOCAL_ALPHA = 0.75
+    Config.N_EPOCH = 20
+    Config.MASK_WEIGHT = 100
+    Config.USE_UNCERTAIN_LOSS = True
+    Config.USE_MASK = True
+    model = get_model(Config.model_name)
+    uncertain_loss = UncertaintyLoss().to(Config.device)
+    params = list(uncertain_loss.parameters()) + list(model.parameters())
+    optimizer = optim.AdamW(params, lr=0.001, weight_decay=0.01)
     # exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=Config.N_EPOCH * len(train_loader) // 3, gamma=0.1)
     lr_scheduler = ReduceLROnPlateau(optimizer, mode='max', factor=0.1, patience=3, verbose=True)
     model = training(model, optimizer, scheduler=lr_scheduler, n_epoch=Config.N_EPOCH, writer=writer,
