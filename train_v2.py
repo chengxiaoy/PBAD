6a0ab30d620ae3937df629903662da55f8bc62a9
cheng ying <chengying@tezign.com>
2020-01-07 18:03:45 +0800

    update

--- train_v2.py
+++ train_v2.py
@@ -234,14 +234,38 @@ if __name__ == '__main__':
     Config.FOCAL_ALPHA = 0.75
     Config.N_EPOCH = 30
     Config.MASK_WEIGHT = 500
-    Config.USE_UNCERTAIN_LOSS = False
+    Config.USE_UNCERTAIN_LOSS = True
     Config.USE_MASK = True
     model = get_model(Config.model_name)
-    optimizer = optim.AdamW(model.parameters(), lr=0.001, weight_decay=0.01)
+    uncertain_loss = UncertaintyLoss().to(Config.device)
+    params = list(uncertain_loss.parameters()) + list(model.parameters())
+    optimizer = optim.AdamW(params, lr=0.001, weight_decay=0.01)
     # exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=Config.N_EPOCH * len(train_loader) // 3, gamma=0.1)
     lr_scheduler = ReduceLROnPlateau(optimizer, mode='max', factor=0.1, patience=3, verbose=True)
     model = training(model, optimizer, scheduler=lr_scheduler, n_epoch=Config.N_EPOCH, writer=writer,
-                     uncertain_loss=None)
+                     uncertain_loss=uncertain_loss)
+    predict(model)
+
+
+    Config.expriment_id = 13
+    writer = SummaryWriter(logdir=os.path.join("board/", str(Config.expriment_id)))
+    Config.model_name = "basic_4"
+    Config.MODEL_SCALE = 4
+    Config.IMG_WIDTH = 2304
+    Config.IMG_HEIGHT = 768
+    Config.FOCAL_ALPHA = 0.75
+    Config.N_EPOCH = 30
+    Config.MASK_WEIGHT = 500
+    Config.USE_UNCERTAIN_LOSS = True
+    Config.USE_MASK = True
+    model = get_model(Config.model_name)
+    uncertain_loss = UncertaintyLoss().to(Config.device)
+    params = list(uncertain_loss.parameters()) + list(model.parameters())
+    optimizer = optim.AdamW(params, lr=0.001, weight_decay=0.01)
+    # exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=Config.N_EPOCH * len(train_loader) // 3, gamma=0.1)
+    lr_scheduler = ReduceLROnPlateau(optimizer, mode='max', factor=0.1, patience=3, verbose=True)
+    model = training(model, optimizer, scheduler=lr_scheduler, n_epoch=Config.N_EPOCH, writer=writer,
+                     uncertain_loss=uncertain_loss)
     predict(model)
 
     # Config.expriment_id = 9
