861f084d4f8bdd105ba5000b55c8036b8d6f12ec
cheng ying <chengying@tezign.com>
2020-01-11 12:02:39 +0800

    use gaussian

--- train_v2.py
+++ train_v2.py
@@ -366,15 +366,42 @@ if __name__ == '__main__':
     # model = training(model, optimizer, scheduler=lr_scheduler, n_epoch=Config.N_EPOCH, writer=writer)
     # predict(model)
 
-
-
-    Config.expriment_id = 30_12
+    #
+    #
+    Config.expriment_id = 30_14
     Config.device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
 
     writer = SummaryWriter(logdir=os.path.join("board/", str(Config.expriment_id)))
     Config.model_name = "dla34_2"
     Config.MODEL_SCALE = 4
-    Config.BATCH_SIZE = 32
+    Config.BATCH_SIZE = 4
+    # Config.IMG_WIDTH = 1536
+    # Config.IMG_HEIGHT = 512
+    Config.FOCAL_ALPHA = 0.75
+    Config.N_EPOCH = 10
+    Config.MASK_WEIGHT = 100
+    Config.USE_UNCERTAIN_LOSS = False
+    Config.USE_MASK = True
+    Config.USE_GAUSSIAN = True
+    model = get_model(Config.model_name)
+    uncertain_loss = UncertaintyLoss().to(Config.device)
+    params = list(uncertain_loss.parameters()) + list(model.parameters())
+    optimizer = optim.AdamW(params, lr=0.0001, weight_decay=0.01)
+
+    # exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=Config.N_EPOCH * len(train_loader) // 3, gamma=0.1)
+    lr_scheduler = ReduceLROnPlateau(optimizer, mode='max', factor=0.1, patience=5, verbose=True)
+    model = training(model, optimizer, scheduler=lr_scheduler, n_epoch=Config.N_EPOCH, writer=writer,
+                     uncertain_loss=uncertain_loss)
+    predict(model)
+    #
+    #
+    Config.expriment_id = 30_15
+    Config.device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+
+    writer = SummaryWriter(logdir=os.path.join("board/", str(Config.expriment_id)))
+    Config.model_name = "dla34_2"
+    Config.MODEL_SCALE = 4
+    Config.BATCH_SIZE = 4
     # Config.IMG_WIDTH = 1536
     # Config.IMG_HEIGHT = 512
     Config.FOCAL_ALPHA = 0.75
@@ -395,53 +422,3 @@ if __name__ == '__main__':
     predict(model)
 
 
-    Config.expriment_id = 30_13
-    Config.device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
-
-    writer = SummaryWriter(logdir=os.path.join("board/", str(Config.expriment_id)))
-    Config.model_name = "dla34_2"
-    Config.MODEL_SCALE = 4
-    Config.BATCH_SIZE = 32
-    # Config.IMG_WIDTH = 1536
-    # Config.IMG_HEIGHT = 512
-    Config.FOCAL_ALPHA = 0.75
-    Config.N_EPOCH = 10
-    Config.MASK_WEIGHT = 100
-    Config.USE_UNCERTAIN_LOSS = False
-    Config.USE_MASK = True
-    Config.USE_GAUSSIAN = True
-    model = get_model(Config.model_name)
-    uncertain_loss = UncertaintyLoss().to(Config.device)
-    params = list(uncertain_loss.parameters()) + list(model.parameters())
-    optimizer = optim.AdamW(params, lr=0.0001, weight_decay=0.01)
-
-    # exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=Config.N_EPOCH * len(train_loader) // 3, gamma=0.1)
-    lr_scheduler = ReduceLROnPlateau(optimizer, mode='max', factor=0.1, patience=5, verbose=True)
-    model = training(model, optimizer, scheduler=lr_scheduler, n_epoch=Config.N_EPOCH, writer=writer,
-                     uncertain_loss=uncertain_loss)
-    predict(model)
-
-    Config.expriment_id = 30_12
-    Config.device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
-
-    writer = SummaryWriter(logdir=os.path.join("board/", str(Config.expriment_id)))
-    Config.model_name = "dla34_2"
-    Config.MODEL_SCALE = 4
-    # Config.IMG_WIDTH = 1536
-    # Config.IMG_HEIGHT = 512
-    Config.FOCAL_ALPHA = 0.75
-    Config.N_EPOCH = 30
-    Config.MASK_WEIGHT = 200
-    Config.USE_UNCERTAIN_LOSS = True
-    Config.USE_MASK = True
-    Config.USE_GAUSSIAN = False
-    model = get_model(Config.model_name)
-    uncertain_loss = UncertaintyLoss().to(Config.device)
-    params = list(uncertain_loss.parameters()) + list(model.parameters())
-    optimizer = optim.AdamW(params, lr=0.001, weight_decay=0.01)
-
-    # exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=Config.N_EPOCH * len(train_loader) // 3, gamma=0.1)
-    lr_scheduler = ReduceLROnPlateau(optimizer, mode='max', factor=0.1, patience=3, verbose=True)
-    model = training(model, optimizer, scheduler=lr_scheduler, n_epoch=Config.N_EPOCH, writer=writer,
-                     uncertain_loss=uncertain_loss)
-    predict(model)
