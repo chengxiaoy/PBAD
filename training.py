a7829af191ffb89cfcc4f6354846528c476987ae
cheng ying <chengying@tezign.com>
2020-01-11 11:50:27 +0800

    use gaussian

--- training.py
+++ training.py
@@ -314,9 +314,10 @@ if __name__ == '__main__':
     Config.USE_MASK = True
     Config.USE_GAUSSIAN = False
     model = get_model(Config.model_name)
+    model.load_state_dict(torch.load('15_model.pth'))
     uncertain_loss = UncertaintyLoss().to(Config.device)
     params = list(uncertain_loss.parameters()) + list(model.parameters())
-    optimizer = optim.AdamW(params, lr=0.001, weight_decay=0.01)
+    optimizer = optim.AdamW(params, lr=0.0001, weight_decay=0.01)
     # exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=Config.N_EPOCH * len(train_loader) // 3, gamma=0.1)
     lr_scheduler = ReduceLROnPlateau(optimizer, mode='max', factor=0.1, patience=3, verbose=True)
     model = training(model, optimizer, scheduler=lr_scheduler, n_epoch=Config.N_EPOCH, writer=writer,
