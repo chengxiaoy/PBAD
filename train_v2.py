ef988bd77c30d19a9d4fc3f2d136410ffec0b2c1
e <chengyingjuly@163.com>
2020-01-21 22:59:30 +0800

    update

--- train_v2.py
+++ train_v2.py
@@ -748,30 +748,53 @@ if __name__ == '__main__':
     #                  uncertain_loss=uncertain_loss)
     # predict(model)
 
-    Config.expriment_id = 48_4
+    # Config.expriment_id = 48_4
+    # writer = SummaryWriter(logdir=os.path.join("board/", str(Config.expriment_id)))
+    # Config.model_name = "basic_4"
+    # Config.MODEL_SCALE = 4
+    # Config.IMG_WIDTH = 2048
+    # Config.IMG_HEIGHT = 768
+    # Config.FOCAL_ALPHA = 0.85
+    # Config.N_EPOCH = 40
+    # Config.MASK_WEIGHT = 1000
+    # uncertain_loss = UncertaintyLoss().to(Config.device)
+    # Config.USE_MASK = True
+    # Config.USE_UNCERTAIN_LOSS = True
+    # model = get_model(Config.model_name)
+    # # model.load_state_dict(torch.load('17_model.pth'))
+    #
+    # params = list(uncertain_loss.parameters()) + list(model.parameters())
+    # optimizer = optim.AdamW(params, lr=0.01)
+    #
+    # # optimizer = optim.AdamW(model.parameters(), lr=0.01, weight_decay=0.01)
+    # # exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=Config.N_EPOCH * len(train_loader) // 3, gamma=0.1)
+    # # lr_scheduler = ReduceLROnPlateau(optimizer, mode='max', factor=0.1, patience=3, verbose=True)
+    # # lr_scheduler = MultiStepLR(optimizer, milestones=[15, 25, 40], gamma=0.1)
+    # lr_scheduler = MultiStepLR(optimizer, milestones=[11, 16, 21], gamma=0.1)
+    #
+    # model = training(model, optimizer, scheduler=lr_scheduler, n_epoch=Config.N_EPOCH, writer=writer,
+    #                  uncertain_loss=uncertain_loss)
+    # predict(model)
+
+
+    #
+    Config.expriment_id = 17_7
     writer = SummaryWriter(logdir=os.path.join("board/", str(Config.expriment_id)))
     Config.model_name = "basic_4"
     Config.MODEL_SCALE = 4
-    Config.IMG_WIDTH = 2048
-    Config.IMG_HEIGHT = 768
-    Config.FOCAL_ALPHA = 0.85
-    Config.N_EPOCH = 40
+    Config.IMG_WIDTH = 1536
+    Config.IMG_HEIGHT = 512
+    Config.FOCAL_ALPHA = 0.8
+    Config.N_EPOCH = 10
     Config.MASK_WEIGHT = 1000
     uncertain_loss = UncertaintyLoss().to(Config.device)
     Config.USE_MASK = True
-    Config.USE_UNCERTAIN_LOSS = True
     model = get_model(Config.model_name)
-    # model.load_state_dict(torch.load('17_model.pth'))
+    model.load_state_dict(torch.load('17_model.pth'))
 
-    params = list(uncertain_loss.parameters()) + list(model.parameters())
-    optimizer = optim.AdamW(params, lr=0.01)
-
-    # optimizer = optim.AdamW(model.parameters(), lr=0.01, weight_decay=0.01)
+    optimizer = optim.AdamW(model.parameters(), lr=0.0001, weight_decay=0.01)
     # exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=Config.N_EPOCH * len(train_loader) // 3, gamma=0.1)
-    # lr_scheduler = ReduceLROnPlateau(optimizer, mode='max', factor=0.1, patience=3, verbose=True)
-    # lr_scheduler = MultiStepLR(optimizer, milestones=[15, 25, 40], gamma=0.1)
-    lr_scheduler = MultiStepLR(optimizer, milestones=[11, 16, 21], gamma=0.1)
-
+    lr_scheduler = ReduceLROnPlateau(optimizer, mode='max', factor=0.1, patience=3, verbose=True)
     model = training(model, optimizer, scheduler=lr_scheduler, n_epoch=Config.N_EPOCH, writer=writer,
                      uncertain_loss=uncertain_loss)
     predict(model)
